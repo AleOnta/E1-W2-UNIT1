3080016f95306c701df0bdbbad77a18f7da761f6
Alessandro Ontani <casualotn@gmail.com>
2022-12-05 17:43:01 +0100

    final update

--- D2.js
+++ D2.js
@@ -79,7 +79,7 @@ Una volta dichiarata e assegnata una costante, il suo valore non cambierà mai.
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
-console.log(4 - x);
+console.log("il risultato è: " + 4 - x);
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
